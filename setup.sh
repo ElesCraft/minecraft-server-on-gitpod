7608360094ba3ef64cc08b96c1e2ac5ac0b8c21c
ElesCraft <130361967+ElesCraft@users.noreply.github.com>
2023-04-10 23:18:13 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
-wget https://pan-1258885526.cos.ap-beijing.myqcloud.com/uploads/2021/01/07/u0CbXu8f_java8.zip -O java8.zip
-unzip java8.zip
-rm java8.zip
-mv java-8-openjdk-amd64/ current/
+wget https://www.dropbox.com/s/st1tmemfvkmz4b6/openlogic-openjdk-17.0.6%2B10-linux-x64.zip?dl=1 -O openlogic-openjdk-17.0.6+10-linux-x64.zip
+unzip openlogic-openjdk-17.0.6+10-linux-x64.zip
+rm openlogic-openjdk-17.0.6+10-linux-x64.zip
+mv openlogic-openjdk-17.0.6+10-linux-x64/ current/
 rm -f /home/gitpod/.sdkman/candidates/java/current
 mv current/ /home/gitpod/.sdkman/candidates/java/
 unset JAVA_TOOL_OPTIONS
+chmod +x /home/gitpod/.sdkman/candidates/java/current/bin/java
 wget https://github.com/fatedier/frp/releases/download/v0.34.3/frp_0.34.3_linux_amd64.tar.gz -O frp.tar.gz
 tar xf frp.tar.gz
 rm frp.tar.gz
@@ -13,9 +14,9 @@ rm -f frp/frpc.ini
 chmod a+x frp/start_frpc.sh
 mkdir minecraft
 cd minecraft
-wget https://launcher.mojang.com/v1/objects/35139deedbd5182953cf1caa23835da59ca3d7cd/server.jar
+wget https://api.papermc.io/v2/projects/paper/versions/1.19.4/builds/507/downloads/paper-1.19.4-507.jar -O  server.jar
 echo 'eula=true' > eula.txt
-echo 'java -Xmx4G -jar server.jar nogui' > start.sh
+echo 'java -Xmx11G -jar server.jar nogui' > start.sh
 chmod a+x start.sh
 echo 'First modify frpc.ini to setup frpc'
 echo 'run start_frp.sh to start frp'
